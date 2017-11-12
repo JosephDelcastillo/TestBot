329c532a03ecbc87d8c36e793edf3a2a65319fce
BCaven <blakecaven@outlook.com>
2017-11-12 13:43:18 -0500

    Update Robot.java
    added comments and safeguard against lost connection

--- TestBot/TestBot/src/org/usfirst/frc/team86/robot/Robot.java
+++ TestBot/TestBot/src/org/usfirst/frc/team86/robot/Robot.java
@@ -1,5 +1,4 @@
-	package org.usfirst.frc.team86.robot;
-
+package org.usfirst.frc.team86.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
@@ -27,9 +26,11 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void autonomousPeriodic() {
 		if (timer.get() < 5) {
+			// go forward at 0.5 speed
 			rightMotor.set(0.5);
 			leftMotor.set(0.5);
 		} else {
+			// stop movement after 5 seconds
 			rightMotor.set(0);
 			leftMotor.set(0);			
 		}
@@ -40,9 +41,15 @@ public class Robot extends IterativeRobot {
 		leftMotor.set(controller.getY());
 		rightMotor.set(controller.getY());
 		if(controller.getX() > 0 ) {
+			// turn left
 			leftMotor.set((controller.getX() + controller.getY())/2);
 		} else if (controller.getX() < 0 ) {
+			// turn right
 			rightMotor.set((controller.getX() - controller.getY())/2 );
+		} else {
+			// have better fine movement control - no drift possible
+			rightMotor.set(0);
+			leftMotor.set(0);
 		}
 	}
 
