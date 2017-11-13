efba1a71d508feac0dee47da373804d849e19f22
1234ViperSting <josephdelcastillo@outlook.com>
2017-11-13 05:59:40 -0500

    11-13

--- TestBot/TestBot/src/org/usfirst/frc/team86/robot/Robot.java
+++ TestBot/TestBot/src/org/usfirst/frc/team86/robot/Robot.java
@@ -1,22 +1,34 @@
 	package org.usfirst.frc.team86.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.XboxController;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI;
 
 public class Robot extends IterativeRobot {
 	
 	public static Victor leftMotor;
 	public static Victor rightMotor;
-	private Timer timer = new Timer();;
-	public XboxController controller = new XboxController(1);
+	
+	public AHRS ahrs = new AHRS(SPI.Port.kMXP);
+	
+	private Timer timer = new Timer();
+	
+	public XboxController controller = new XboxController(0);
 
 	@Override	
 	public void robotInit() {
 		rightMotor = new Victor(0);
-		leftMotor.setInverted(true);
 		leftMotor = new Victor(1);
+		leftMotor.setInverted(true);
+		
+		ahrs.reset();
 	}
 
 	@Override
@@ -26,24 +38,20 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void autonomousPeriodic() {
-		if (timer.get() < 5) {
-			rightMotor.set(0.5);
-			leftMotor.set(0.5);
-		} else {
+		
+		if (ahrs.getAngle() < 90) {
+			leftMotor.set(-0.15);
+			rightMotor.set(0.15);
+		}else {
 			rightMotor.set(0);
-			leftMotor.set(0);			
+			leftMotor.set(0);
 		}
 	}
 	
 	@Override
 	public void teleopPeriodic() {
-		leftMotor.set(controller.getY());
-		rightMotor.set(controller.getY());
-		if(controller.getX() > 0 ) {
-			leftMotor.set((controller.getX() + controller.getY())/2);
-		} else if (controller.getX() < 0 ) {
-			rightMotor.set((controller.getX() - controller.getY())/2 );
-		}
+		leftMotor.set((-controller.getX() + controller.getY())/2);
+		rightMotor.set((controller.getX() + controller.getY())/2 );		
 	}
 
 	@Override
